745317c4b50d1695d43e5e095ea914b2c5596ba7
YONGJUN CHO <as2686@naver.com>
2023-01-15 23:23:53 +0900

    BOJ 9251 LCS

---
+++ src/BackJoon/DynamicProgramming/BOJ9251.java
@@ -0,0 +1,36 @@
+package BackJoon.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// LCS
+public class BOJ9251 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+
+
+        int dp[][] = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i < s1.length() + 1; i++) {
+            for (int j = 1; j < s2.length() + 1; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + 1, dp[i][j - 1]);
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        System.out.println(dp[s1.length()][s2.length()]);
+
+    }
+
+
+}
