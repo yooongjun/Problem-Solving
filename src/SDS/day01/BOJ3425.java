30e1350d61afb06ea3269637175ec5c4addbbf99
Yongjun Cho <105872347+yooongjun@users.noreply.github.com>
2023-01-30 22:13:30 +0900

    Update BOJ3425.java

--- src/SDS/day01/BOJ3425.java
+++ src/SDS/day01/BOJ3425.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Stack;
 
-// 게임
+// 고스택
 public class BOJ3425 {
 
     public static void main(String[] args) throws IOException {
