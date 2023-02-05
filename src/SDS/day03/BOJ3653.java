f8a42458464408c221b4a254bae074c8afe39c85
YONGJUN CHO <as2686@naver.com>
2023-02-05 17:47:49 +0900

    idx tree practice

--- src/SDS/day03/BOJ3653.java
+++ src/SDS/day03/BOJ3653.java
@@ -1,18 +1,66 @@
 package SDS.day03;
 
+import javax.xml.transform.sax.SAXSource;
 import java.io.*;
 import java.util.StringTokenizer;
 
 public class BOJ3653 {
 
     static int n,m;
-    static long idx[] = new long[300000];
+    static int idx[];
     static int firstLeaf = 1;
 
-    static void edit(int index){
-        int x = 1;
+    // 노드의 현재 위치 저장
+    static int dt[];
 
+    // 가장 높은 층
+    static int top;
+
+    static void edit(int x){
+
+        if(dt[x] == top){
+            return;
         }
+
+        int t = firstLeaf + dt[x] - 1;
+        idx[t] = 0;
+
+        // 해당 층에서 제거
+        while (t > 0){
+            t /= 2;
+            idx[t] = idx[t * 2] + idx[t * 2 + 1];
+        }
+
+        // top에 쌓기
+        top++;
+
+        dt[x] = top;
+
+        t = firstLeaf + dt[x] - 1;
+        idx[t] = 1;
+        t /= 2;
+
+        while (t > 0){
+            idx[t] = idx[t * 2] + idx[t * 2 + 1];
+            t /= 2;
+        }
+    }
+
+    // x 위에 있는 영화 수
+    static long sum(int x, int l, int r, int search_l, int search_r) {
+
+        // 구간에 겹치는 부분이 하나도 없는 경우
+        if( l > search_r || r < search_l)
+            return 0;
+
+        // 현재 노드의 구간이 모두 겹치는 경우
+        if( l >= search_l && r <= search_r) {
+            return idx[x];
+        }
+
+        return sum(x*2, l, (l+r)/2 , search_l, search_r) +
+                sum(x*2+1, (l+r)/2 + 1, r, search_l, search_r);
+    }
 
 
     public static void main(String[] args) throws IOException {
@@ -21,28 +69,34 @@ public class BOJ3653 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t;
-
-        // 빈 idx 트리 생성
-        while(firstLeaf < 100000){
-            firstLeaf *= 2;
-        }
-
         t = Integer.parseInt(st.nextToken());
 
         for(int i =0; i <t; i++){
-            firstLeaf = 1;
+
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
 
-            while(firstLeaf < n){
+            dt = new int[n+m+1];
+            firstLeaf = 1;
+
+            while(firstLeaf < n + m){
                 firstLeaf *= 2;
             }
 
-            // 초기 데이터 입력 -> 내 앞에 있는 DVD 수
+            idx = new int[2 * (firstLeaf) + 1];
+
+            int tmp = 1;
+            // 데이터 입력
             for(int j = firstLeaf; j <= firstLeaf + n - 1; j++){
-                idx[j] = j - firstLeaf;
+                idx[j] = 1;
+
+                // 영화의 위치를 저장
+                dt[tmp++] = firstLeaf + n - j;
             }
+
+            top = dt[1];
+
 
             for(int j = firstLeaf -1; j > 0; j--){
                 idx[j] = idx[j * 2] + idx[j * 2 + 1];
@@ -51,12 +105,13 @@ public class BOJ3653 {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j < m - 1; j++){
                 int a = Integer.parseInt(st.nextToken());
-                bw.append(idx[firstLeaf + a -1] + " ");
-                edit(firstLeaf + a -1);
+                bw.append(sum(1, 1, firstLeaf, dt[a] + 1, top) + " ");
+                edit(a);
             }
 
             int a = Integer.parseInt(st.nextToken());
-            bw.append(String.valueOf(idx[firstLeaf + a - 1]));
+
+            bw.append(String.valueOf(sum(1, 1, firstLeaf, dt[a] + 1, top)));
             bw.append("\n");
         }
         bw.flush();
