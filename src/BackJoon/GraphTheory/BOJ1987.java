e2a2d0faa6efefd5b417fa0e1009194a2afb9c95
YONGJUN CHO <as2686@naver.com>
2023-04-05 19:42:05 +0900

    BOJ 1897 알파벳

--- src/BackJoon/GraphTheory/BOJ1987.java
+++ src/BackJoon/GraphTheory/BOJ1987.java
@@ -3,8 +3,6 @@ package BackJoon.GraphTheory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * 알파벳
