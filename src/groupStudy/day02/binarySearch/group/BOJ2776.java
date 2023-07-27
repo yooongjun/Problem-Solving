5b04350f72c274a37dcafce55189813b9ae30453
yooongjun <as2686@naver.com>
2023-07-28 04:22:08 +0900

    add description

--- src/groupStudy/day02/binarySearch/group/BOJ2776.java
+++ src/groupStudy/day02/binarySearch/group/BOJ2776.java
@@ -42,6 +42,8 @@ public class BOJ2776 {
             Arrays.sort(arr_1);
 
             for (int i = 0; i < M; i++) {
+                // bufferedWriter.append()는 결과를 출력에 담는 함수입니다.
+                // arr_2의 각 원소를 기준으로 arr_1을 이분탐색했습니다.
                 bufferedWriter.append(binarySearch(arr_1, arr_2[i]) + "\n");
             }
 
@@ -54,27 +56,32 @@ public class BOJ2776 {
         bufferedWriter.close();
     }
 
+    // int[] target : 탐색할 배열, find : 찾을 수
     static int binarySearch(int[] target, int find) {
+
+        // left , right
         int l = 0;
         int r = target.length - 1;
 
         while (l <= r) {
+            // 중간 값을 비교
             int mid = (l+r)/2;
 
-            // 찾은 경우 1을 리턴
+            // 같은 값을 찾은 경우 1을 리턴
             if (find == target[mid]) {
                 return 1;
             }
 
+            // 중간 값보다 찾는 수가 작은 경우
             if (find < target[mid]) {
                 r = mid - 1;
             }
-            else // if ( find > target[mid] )
+            else // if ( find > target[mid] ) 중간 값보다 찾는 수가 큰 경우
             {
                 l = mid + 1;
             }
         }
-        
+
         // 찾지 못한 경우
         return 0;
     }
