811c842f4a9d0d47827b9603b3ecf39611e80859
yooongjun <as2686@naver.com>
2023-08-07 04:00:27 +0900

    BOJ 2217 로프

---
+++ src/groupStudy/day03/greedy/group/BOJ2217.java
@@ -0,0 +1,46 @@
+package groupStudy.day03.greedy.group;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 로프
+ */
+public class BOJ2217 {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bufferedReader.readLine());
+
+
+        Integer[] ropes = new Integer[N];
+
+        for (int i = 0; i < ropes.length; i++) {
+            int length = Integer.parseInt(bufferedReader.readLine());
+            ropes[i] = length;
+        }
+
+        // 로프의 길이로 내림차순 정렬
+        Arrays.sort(ropes, Collections.reverseOrder());
+
+        int maxWeight = 0;
+
+        for (int i = 0; i < ropes.length; i++) {
+
+            // i번째 로프를 사용하는 경우 최대 무게
+            int curWeight = (i + 1) * ropes[i];
+
+            if( maxWeight >= curWeight ) break;
+
+            maxWeight = curWeight;
+        }
+
+        System.out.println(maxWeight);
+    }
+
+}
