c46f264d466f3815ed11d154ec44ed7ffb602282
JUN <as2686@naver.com>
2024-04-12 14:18:39 +0900

    BOJ14426 접두사 찾기

--- src/BackJoon/BOJ14426.java
+++ src/BackJoon/BOJ14426.java
@@ -3,34 +3,60 @@ package BackJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 // 접두사 찾기
 public class BOJ14426 {
 
     static int N, M;
-    static String[] s1;
-    static String[] s2;
+    static Node trie = new Node(); // trie의 루트 노드
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String[] s = bufferedReader.readLine().split(" ");
 
+        int cnt = 0;
         N = Integer.parseInt(s[0]);
         M = Integer.parseInt(s[1]);
 
-        s1 = new String[N];
-        s2 = new String[M];
-
         for (int i = 0; i < N; i++) {
-            s1[i] = bufferedReader.readLine();
+            insertNode(bufferedReader.readLine());
         }
 
         for (int i = 0; i < M; i++) {
-            s2[i] = bufferedReader.readLine();
+            if (searchNode(bufferedReader.readLine())) {
+               cnt++;
+            }
         }
 
+        System.out.println(cnt);
+    }
 
+    static boolean searchNode(String s) {
 
+        Node node = trie;
 
+        for (int i = 0; i < s.length(); i++) {
+            if (node.child.containsKey(s.charAt(i))) {
+                node = node.child.get(s.charAt(i));
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void insertNode(String s) {
+
+        Node node = trie;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            node = node.child.computeIfAbsent(c, k -> new Node());
+        }
+    }
+
+    static class Node{
+        HashMap<Character, Node> child = new HashMap<>();
     }
 }
